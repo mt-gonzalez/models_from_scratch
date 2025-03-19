a0a962bb6324e574b6429f4d20ca311e02bbfb98
Matias Gonzalez <matiastomasglez@gmail.com>
2025-03-19 14:15:34 -0300

    Fix in distance formulas, forgot the return

--- knn/knn.py
+++ knn/knn.py
@@ -4,13 +4,16 @@ from collections import Counter
 
 # Formulas for distances calculations
 def manhattan_distance(x_1, x_2):
-    np.sum(np.abs(x_1 - x_2))
+    d = np.sum(np.abs(x_1 - x_2))
+    return d
 
 def euclidean_distance(x_1, x_2):
-    np.sqrt(np.sum((x_1 - x_2)**2))
+    d = np.sqrt(np.sum((x_1 - x_2)**2))
+    return d
 
 def cosine_distance(x_1, x_2):
-    1 - np.dot(x_1, x_2)/(norm(x_1)*norm(x_2))
+    d = 1 - np.dot(x_1, x_2)/(norm(x_1)*norm(x_2))
+    return d
 
 class KNN:
     def __init__(self, k=5):
@@ -23,27 +26,31 @@ class KNN:
         self.X_train = X_train
         self.Y_train = Y_train
 
-    def get_neighbours(self, x_0, d_formula=euclidean_distance):
+    def gather_distances(self, x_0, d_formula=euclidean_distance):
         distances = []
-        for x_train in self.X_train:
-            distances.append(d_formula(x_0, x_train))
+        for x in self.X_train:
+            distances.append(d_formula(x_0, x))
         
+        return distances
+    
+    def get_neighbours(self, distances):
         distances = np.array(distances)
         sorted_distances = np.argsort(distances)
         neighbours = self.Y_train[sorted_distances[:self.k]] # We took the k-nearest neighbours
 
+        neighbours = np.array(neighbours) # Convert neighbours to a np.array
         return neighbours
     
-    def most_common(self, k_neighbours):
-        k_neighbours = Counter(self.get_neighbours(x_0))
+    def most_common(self, neighbours): # Receives a np.array
+        k_neighbours = Counter(neighbours) 
 
         most_common, most_common_count = k_neighbours(1)[0]
         #I want to know how many neighbours share the top
         top_commons = len(count for count in k_neighbours.values() if count == most_common_count)
         if top_commons == 1:
-            return most_common
+            return most_common # If there is one top neighbours we return it
         else:
-            return most_common(k_neighbours[:-1])
+            return most_common(k_neighbours[:-1]) # Otherwise we slice the last neighbour and count again
     
     def predict(self, x_0):
         predicted_class = self.most_common(self.get_neighbours(x_0))
