69b57b0443460b450a45a1dd7eab289b1e7ec14e
Matias Gonzalez <matiastomasglez@gmail.com>
2025-03-19 18:27:01 -0300

    begin same implementation in R

---
+++ knn/knn.R
@@ -0,0 +1,54 @@
+# Distances Formulas
+manhattan_distance <- function(x_1, x_2) {
+  d <- sum(abs(x_1 - x_2))
+  
+  return(d)
+}
+
+euclidean_distance <- function(x_1, x_2) {
+  d <- sum(sqrt((x_1 - x_2)^2))
+  
+  return(d)
+}
+
+cosine_distance <- function(x_1, x_2) {
+  dot_product <- sum(x_1*x_2)
+  
+  norm_x1 <- sqrt(sum(x_1^2))
+  norm_x2 <- sqrt(sum(x_2^2))
+  
+  similarity <- dot_product / (norm_x1 * norm_x2)
+  d <- (1 - similarity)
+  
+  if (abs(d) < 1e-15) { # Managing numerical error
+    return(0)
+  }
+  
+  return(d)
+}
+
+# Class creation
+create_model <- function(k=3) {
+  knn_model = list(
+    k = k,
+    X_train = NULL,
+    Y_train = NULL
+  )
+  
+  class(knn_model) <- "KNN"
+  return(knn_model)
+
+}
+
+fit.KNN <- function(model, X_train, Y_train) {
+  model$X_Train <- X_train
+  model$Y_train <- Y_train
+  
+  return(model)
+}
+
+gather_distances.KNN <- function(model, x_0, d_formula = euclidean_distance) {
+  distances <- apply(model$X_train, 1, function(x_train) d_formula(x_0, x_train))
+  
+  return(distances)
+}
