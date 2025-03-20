d06fa48ac695a7d27f7d1a615a6d29eb2a1b2a51
Matias Gonzalez <matiastomasglez@gmail.com>
2025-03-19 21:12:42 -0300

    finished knn.r, non-tested

--- knn/knn.R
+++ knn/knn.R
@@ -52,3 +52,25 @@ gather_distances.KNN <- function(model, x_0, d_formula = euclidean_distance) {
   
   return(distances)
 }
+
+getNeighbours.KNN <- function(model, distances) {
+  sorted_indices <- order(distances)
+  neighbours <- model$Y_train[sorted_indices[1:model$k]]
+  
+  return(neighbours)
+}
+
+most_common.KNN <- function(model, neighbours) {
+  neighbours_count <- table(neighbours)
+  most_common <- names(neighbours_count)[which.max(neighbours_count)]
+  
+  return(most_common)
+}
+
+predict.KNN <- function(model, x_0) {
+  distances <- gather_distances.KNN(model, x_0)
+  k_neighbours <- getNeighbours.KNN(model, distances)
+  predicted_class <- most_common.KNN(model, k_neighbours)
+  
+  return(predicted_class)
+}
